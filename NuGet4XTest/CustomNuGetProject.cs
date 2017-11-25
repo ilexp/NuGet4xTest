a530dde19708442d22357e7cffaef8a92756e5f4
Adam <adam@adamslair.net>
2017-11-25 13:34:42 +0100

    Custom Project GetInstalledPackages

--- NuGet4XTest/CustomNuGetProject.cs
+++ NuGet4XTest/CustomNuGetProject.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.Xml.Linq;
 
 using NuGet.Common;
 using NuGet.Protocol.Core.Types;
@@ -17,15 +18,17 @@ using NuGet.Packaging.Core;
 using NuGet.PackageManagement;
 using NuGet.ProjectManagement;
 using NuGet.Resolver;
-using System.Xml.Linq;
+using NuGet.Versioning;
 
 namespace NuGet4XTest
 {
 	public class CustomNuGetProject : FolderNuGetProject
 	{
+		private NuGetFramework framework;
+
 		public CustomNuGetProject(string path, NuGetFramework framework) : base(path, new PackagePathResolver(path), framework)
 		{
-
+			this.framework = framework;
 		}
 
 		public override Task PreProcessAsync(INuGetProjectContext nuGetProjectContext, CancellationToken token)
@@ -41,7 +44,46 @@ namespace NuGet4XTest
 		public override Task<IEnumerable<PackageReference>> GetInstalledPackagesAsync(CancellationToken token)
 		{
 			Console.WriteLine("GetInstalledPackagesAsync");
-			return base.GetInstalledPackagesAsync(token);
+
+			List<PackageReference> installedPackages = new List<PackageReference>();
+			foreach (string packageFolder in Directory.EnumerateDirectories(this.Root))
+			{
+				string packageFolderName = Path.GetFileName(packageFolder);
+				string packageFileName = packageFolderName + ".nupkg";
+				string packageFilePath = Path.Combine(packageFolder, packageFileName);
+				if (!File.Exists(packageFilePath)) continue;
+
+				int dotIndex = packageFolderName.Length;
+				int dotCount = 0;
+				while (true)
+				{
+					dotIndex = packageFolderName.LastIndexOf('.', dotIndex - 1);
+					if (dotIndex == -1) break;
+
+					dotCount++;
+					if (dotCount < 3) continue;
+
+					string potentialVersionString = packageFolderName.Substring(
+						dotIndex + 1, 
+						packageFolderName.Length - dotIndex - 1);
+					NuGetVersion version;
+					if (!NuGetVersion.TryParse(potentialVersionString, out version))
+						continue;
+
+					string packageName = packageFolderName.Remove(dotIndex);
+					PackageIdentity identity = new PackageIdentity(packageName, version);
+
+					installedPackages.Add(new PackageReference(identity, this.framework));
+					break;
+				}
+			}
+
+			foreach (PackageReference item in installedPackages)
+			{
+				Console.WriteLine("- {0}", item);
+			}
+
+			return Task.FromResult(installedPackages as IEnumerable<PackageReference>);
 		}
 		public override Task<bool> InstallPackageAsync(PackageIdentity packageIdentity, DownloadResourceResult downloadResourceResult, INuGetProjectContext nuGetProjectContext, CancellationToken token)
 		{
